1aa994c8fe2b947b3c361f6649c723d9de9eb44a
yun1113 <yunyun1113c@gmail.com>
2016-11-14 11:13:19 +0800

    fix urls and tab indent

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.dashboard),
-    url(r'^login/', views.login),
-    url(r'^register/', views.register),
+    url(r'^dashboard$', views.dashboard),
+    url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
 ]
