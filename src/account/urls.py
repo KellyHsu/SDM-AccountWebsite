f82a84b9c64ef00a6a9c4bca4c39377c640027b7
r05725014 <r05725014@ntu.edu.tw>
2016-11-28 17:15:09 +0800

    ajax create receipt

--- src/account/urls.py
+++ src/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^dashboard/$', views.dashboard),
     url(r'^setting/$', views.setting),
     url(r'^filter/$', views.filter),
+    url(r'^createReceipt/$', views.createReceipt, name='createReceipt'),
 ]
--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,4 @@
+#coding=utf8 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -18,29 +19,33 @@ def filter(request):
 	return render(request, 'filter.html', {})
 
 def createReceipt(request):
-	member = request.POST['user']
-	#get user?
-	money = request.POST['amount']
-	remark = request.POST['memo']
-	date = request.POST['datepicker1']
-	subclassification = request.POST['costCategory'] 
-	#only need sub category
-	payment = request.POST['payment']
-	if payment == 'cash':
-		payment = 'CR'
-	elif payment == 'credit-card':
-		payment = 'CA'
-	elif payment == 'other':
-		payment = 'OT'
-	
-	incomeandexpense = request.POST['recordType']
-	if incomeandexpense == 'cost':
-		incomeandexpense = 'EX'
-	elif incomeandexpense == 'revenue':
-		incomeandexpense = 'IN'
-
-	account.objects.create(member=member, usermoney=money, remark=remark, date=date, subclassification=subclassification, payment=payment, incomeandexpense=incomeandexpense)
-	return render(request, 'dashboard.html', {})
+	print "test view"
+	if request.method == 'POST':
+		print "it's a test"
+		member = request.POST['member']
+		print member
+		money = request.POST['money']
+		remark = request.POST['remark']
+		date = request.POST['date']
+		subclassification = request.POST['subclassification'] 
+		#only need sub category
+		payment = request.POST['payment']
+		if payment == 'cash':
+			payment = 'CR'
+		elif payment == 'credit-card':
+			payment = 'CA'
+		elif payment == 'other':
+			payment = 'OT'
+		incomeandexpense = request.POST['incomeandexpense']
+		if incomeandexpense == 'cost':
+			incomeandexpense = 'EX'
+		elif incomeandexpense == 'revenue':
+			incomeandexpense = 'IN'
+		return HttpResponse("success")
+	else:
+		return HttpResponse("<h1>test</h1>")  
+		#account.objects.create(member=member, usermoney=money, remark=remark, date=date, subclassification=subclassification, payment=payment, incomeandexpense=incomeandexpense)
+		#return render(request, 'dashboard.html', {})
 
 def createSubClassification(request):
 	member = request.POST['user']
