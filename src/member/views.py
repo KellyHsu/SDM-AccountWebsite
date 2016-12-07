54b7232d843275dc4b2d229c455a46549ae7ce61
r05725014 <r05725014@ntu.edu.tw>
2016-12-07 11:24:57 +0800

    Feature:1.新增classification收入的三個欄位 2.建立新帳號如果沒有classification,payment,incomeandexpense的內容會一起建立

--- src/member/views.py
+++ src/member/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Member
-from account.models import Classification, Budget, MonthBudget
+from account.models import IncomeAndExpense, Payment, Classification, Budget, MonthBudget
 from datetime import datetime
 
 
@@ -14,13 +14,16 @@ def register(request):
             member = Member.objects.create(user=user, create_date=datetime.now())
             MonthBudget.objects.create(budget=0, reminder=0, member=member, is_reminded=False)
             
-            c1 = Classification.objects.filter(classification_type='food').first()
-            c2 = Classification.objects.filter(classification_type='clothing').first()
-            c3 = Classification.objects.filter(classification_type='housing').first()
-            c4 = Classification.objects.filter(classification_type='transportation').first()
-            c5 = Classification.objects.filter(classification_type='education').first()
-            c6 = Classification.objects.filter(classification_type='entertainment').first()
-            c7 = Classification.objects.filter(classification_type='others').first()
+            c1, created1 = Classification.objects.get_or_create(classification_type='food')
+            c2, created2 = Classification.objects.get_or_create(classification_type='clothing')
+            c3, created3 = Classification.objects.get_or_create(classification_type='housing')
+            c4, created4 = Classification.objects.get_or_create(classification_type='transportation')
+            c5, created5 = Classification.objects.get_or_create(classification_type='education')
+            c6, created6 = Classification.objects.get_or_create(classification_type='entertainment')
+            c7, created7 = Classification.objects.get_or_create(classification_type='others')
+            c8, created8 = Classification.objects.get_or_create(classification_type='revenue')
+            c9, created9 = Classification.objects.get_or_create(classification_type='invest_revenue')
+            c10, created10 = Classification.objects.get_or_create(classification_type='other_revenue')
             Budget.objects.create(budget=0, reminder=0, classification=c1, member=member, is_reminded=False)
             Budget.objects.create(budget=0, reminder=0, classification=c2, member=member, is_reminded=False)
             Budget.objects.create(budget=0, reminder=0, classification=c3, member=member, is_reminded=False)
@@ -28,6 +31,13 @@ def register(request):
             Budget.objects.create(budget=0, reminder=0, classification=c5, member=member, is_reminded=False)
             Budget.objects.create(budget=0, reminder=0, classification=c6, member=member, is_reminded=False)
             Budget.objects.create(budget=0, reminder=0, classification=c7, member=member, is_reminded=False)
+
+            p1, createdp1 = Payment.objects.get_or_create(payment_type='credit_card')
+            p2, createdp2 = Payment.objects.get_or_create(payment_type='cash')
+            p3, createdp3 = Payment.objects.get_or_create(payment_type='other')
+
+            i1, createdi1 = IncomeAndExpense.objects.get_or_create(income_type='income')
+            i2, createdi2 = IncomeAndExpense.objects.get_or_create(income_type='expense')
 
             return HttpResponseRedirect('/login/')
     else:
