ad87e7568bcb7570267bf1bef490bc4803d366a1
r05725014 <r05725014@ntu.edu.tw>
2016-12-26 21:32:22 +0800

    feature: 加上Authorization Token

--- src/website/settings.py
+++ src/website/settings.py
@@ -46,6 +46,7 @@ THIRD_PARTY_APPS = (
     'compressor',
     'crispy_forms',
     'rest_framework',
+    'rest_framework.authtoken',
 )
 
 LOCAL_APPS = (
@@ -157,7 +158,10 @@ LOGOUT_REDIRECT_URL = "/login/"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
--- src/website/urls.py
+++ src/website/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from member.views import register
 from django.contrib.auth.views import login, logout
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
@@ -28,4 +29,5 @@ urlpatterns = [
     url(r'^logout/$', logout),
     url(r'^register/$', register),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token),
 ]
